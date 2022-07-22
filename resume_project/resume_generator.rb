5573d6e325b9554b3a865dbe6438b8fb43b981e8
ShobhitWK <shobhit@webkorps.com>
2022-07-23 04:58:07 +0530

    removed modules for now

--- resume_project/resume_generator.rb
+++ resume_project/resume_generator.rb
@@ -2,8 +2,6 @@ PHONE_NUMBER_REGEX = /^\(?[\d]{3}\)?[\s|-]?[\d]{3}-?[\d]{4}$/
 EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 DOB_REGEX = /\d{4}-(0[1-9]|1[0-2])-/
 
-require_relative = "mod.rb"
-
 class ResumeGenerator
     def initialize
         @resume_values = Hash.new
